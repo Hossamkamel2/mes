894812949d5aa0aee19e21f44af23db287a96769
Hossamkamel2 <hossamkamel15@gmail.com>
2021-05-27 14:51:22 +0200

    procfile

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const path = require("path");
 const app = express();
 app.use(express.static(__dirname + "/dist/mes"));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/mes/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/index.html"));
 });
 app.listen(process.env.PORT || 8080);
